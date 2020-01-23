c45853f15e4eb563e95aac9c4c4db5b0602826dc
cjvanlissa <c.j.vanlissa@uu.nl>
2020-01-23 08:20:17 +0100

    Exclude mplus from travis checks

--- tests/testthat/test-syntax_cfa_mplus_lavaan.R
+++ tests/testthat/test-syntax_cfa_mplus_lavaan.R
@@ -1,4 +1,3 @@
-library(MplusAutomation)
 df <- HolzingerSwineford1939
 
 names(df)[grepl("^x", names(df))] <- c("vis_1", "vis_2", "vis_3", "tex_1", "tex_2", "tex_3", "spe_1", "spe_2", "spe_3")
@@ -6,14 +5,15 @@ dict <- get_dictionary(df)
 measurement(dict) -> model
 
 res_lav <- sem(as.lavaan(model), data = df)
-
-res_mplus <- mplusModeler(mplusObject(MODEL = as.mplus(model), rdata = df), modelout = "test.inp", run = 1L)
-
 sm_lv <- summary(res_lav)
-sm_mp <- summary(res_mplus)
-
 table_results(res_lav)
-table_results(res_mplus, all = T)
+
+if(getOption("test_mplus")){
+  library(MplusAutomation)
+  res_mplus <- mplusModeler(mplusObject(MODEL = as.mplus(model), rdata = df), modelout = "test.inp", run = 1L)
+  sm_mp <- summary(res_mplus)
+  table_results(res_mplus, all = T)
+  coef(res_mplus)
+}
 
 
-coef(res_mplus)
