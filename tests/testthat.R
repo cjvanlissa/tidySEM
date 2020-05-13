03852b26e92f16840698d377f0610067827e7da2
myname <my@email.com>
2020-05-13 08:11:26 +0200

    use mplusautomation to check for mplus

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,11 @@
 library(testthat)
 library(tidySEM)
-options("test_mplus" = FALSE)
-if(file.exists("tests/test_local.R")) source("tests/test_local.R")
+library(MplusAutomation)
+if(mplusAvailable(TRUE) == 0){
+  options("test_mplus" = TRUE)
+} else {
+  options("test_mplus" = FALSE)
+}
 # To run all mplus tests, run the code below once to create a file that is not
 # tracked by git, which toggles the option to run mplus tests:
 # writeLines('options("test_mplus" = TRUE)', con = "tests/test_local.R")
