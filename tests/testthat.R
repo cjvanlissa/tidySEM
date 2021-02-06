e533e3c8c5f982ec339feab9a2f56156f6dc033c
cjvanlissa <c.j.vanlissa@uu.nl>
2021-02-06 10:00:03 +0100

    remove local test

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,19 +1,4 @@
 library(testthat)
 library(tidySEM)
 library(MplusAutomation)
-if(mplusAvailable(TRUE) == 0){
-  options("test_mplus" = TRUE)
-} else {
-  options("test_mplus" = FALSE)
-}
-# To run all mplus tests, run the code below once to create a file that is not
-# tracked by git, which toggles the option to run mplus tests:
-# writeLines('options("test_mplus" = TRUE)', con = "tests/test_local.R")
 test_check("tidySEM")
-# Clean up after test_mplus
-if(getOption("test_mplus")){
-    remove_files <- list.files(pattern = "\\.(inp|out|dat|log)$", full.names = TRUE)
-    if(length(remove_files) > 0){
-        invisible(file.remove(remove_files))
-    }
-}
--- tests/testthat/test-syntax_cfa_mplus_lavaan.R
+++ tests/testthat/test-syntax_cfa_mplus_lavaan.R
@@ -1,22 +1,36 @@
 library(lavaan)
-df <- HolzingerSwineford1939
+library(MplusAutomation)
 
-names(df)[grepl("^x", names(df))] <- c("vis_1", "vis_2", "vis_3", "tex_1", "tex_2", "tex_3", "spe_1", "spe_2", "spe_3")
-dict <- tidy_sem(df)
-measurement(dict, meanstructure = TRUE) -> model
+test_that("CFA gives same results in lavaan and Mplus", {
+  the_test <- "compare_mplus_lavaan"
+  old_wd <- getwd()
+  test_dir <- file.path(tempdir(), the_test)
+  dir.create(test_dir)
+  setwd(test_dir)
+  on.exit({unlink(test_dir, recursive = TRUE); setwd(old_wd)}, add = TRUE)
 
-res_lav <- sem(as_lavaan(model), data = df)
+  df <- HolzingerSwineford1939
 
-tb_lav <- table_results(res_lav, columns = NULL)
+  names(df)[grepl("^x", names(df))] <- c("vis_1", "vis_2", "vis_3", "tex_1", "tex_2", "tex_3", "spe_1", "spe_2", "spe_3")
+  dict <- tidy_sem(df)
 
-if(getOption("test_mplus")){
-  library(MplusAutomation)
-  res_mplus <- mplusModeler(mplusObject(MODEL = as_mplus(model), rdata = df), modelout = "test.inp", run = 1L)
+  expect_true(all(dict$dictionary$scale[-c(1:6)] == rep(c("vis", "tex", "spe"), each = 3)))
 
-  tb_mplus <- table_results(res_mplus, columns = NULL)
+  measurement(dict, meanstructure = TRUE) -> model
 
-  test_that("CFA gives same results in lavaan and Mplus", {
+  res_lav <- sem(as_lavaan(model), data = df)
+
+  tb_lav <- table_results(res_lav, columns = NULL)
+  expect_true(nrow(tb_lav) == 36)
+
+  if(isTRUE(mplusAvailable() == 0)){
+    res_mplus <- mplusModeler(mplusObject(MODEL = as_mplus(model), rdata = df), modelout = "test.inp", run = 1L)
+
+    tb_mplus <- table_results(res_mplus, columns = NULL)
+
+
     expect_true(all(gsub("^intercepts", "means", gsub("^residual.", "", tolower(tb_mplus$label))) %in% tolower(tb_lav$label)))
-  })
 
-}
+
+  }
+})
