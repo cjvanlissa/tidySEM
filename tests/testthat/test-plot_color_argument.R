2d7ef2a7599d7fc8c336396f9b6da6594b0fdae7
cjvanlissa <c.j.vanlissa@uu.nl>
2021-02-18 14:35:07 +0100

    Fix tests

--- tests/testthat/test-plot_color_argument.R
+++ tests/testthat/test-plot_color_argument.R
@@ -1,7 +1,18 @@
+library(ggplot2)
 test_that("color argument works for graph_sem", {
+  the_test <- "plot_color"
+  old_wd <- getwd()
+  test_dir <- file.path(tempdir(), the_test)
+  dir.create(test_dir)
+  setwd(test_dir)
+  on.exit({unlink(test_dir, recursive = TRUE); setwd(old_wd)}, add = TRUE)
+
   p <- prepare_graph(layout = get_layout("x", rows = 1))
-  p <- edit_graph(p, {color = "blue"}, element = "nodes")
-  p <- plot(p)
-  expect_known_hash(p, hash = "a83fd8931a")
+  p1 <- edit_graph(p, {color = "blue"}, element = "nodes")
+  p2 <- edit_graph(p, {colour = "blue"}, element = "nodes")
+  ggsave("p1.png", plot(p1), device = "png")
+  ggsave("p2.png", plot(p2), device = "png")
+
+  expect_equivalent(tools::md5sum("p1.png"), tools::md5sum("p2.png"))
 
 })
---
+++ tests/testthat/test-results_digits.R
@@ -0,0 +1,27 @@
+library(lavaan)
+library(MplusAutomation)
+
+test_that("digits are respected in table_results", {
+
+  fit <- sem("mpg ~ cyl\nmpg ~ am", data = mtcars)
+  expect_error(table_results(fit), NA)
+
+  out <- table_results(fit, columns = c("label", "est_std", "confint"), digits = 3)
+  expect_equivalent(sapply(out, function(x){max(nchar(x))}), c(13, 6, 16))
+
+
+  if(isTRUE(mplusAvailable() == 0)){
+    the_test <- "digits"
+    old_wd <- getwd()
+    test_dir <- file.path(tempdir(), the_test)
+    dir.create(test_dir)
+    setwd(test_dir)
+    on.exit({unlink(test_dir, recursive = TRUE); setwd(old_wd)}, add = TRUE)
+
+    res_mplus <- mplusModeler(mplusObject(MODEL = "mpg ON cyl am;", OUTPUT = "standardized;", rdata = mtcars), modelout = "test.inp", run = 1L)
+    expect_error(table_results(res_mplus), NA)
+    out <- table_results(res_mplus, columns = c("label", "est_std", "confint"), digits = 1)
+    expect_equivalent(sapply(out, function(x){max(nchar(x))}), c(22, 4, 12))
+
+  }
+})
