e3428db732f2130154fa13de34372abcf9d1b32b
cjvanlissa <c.j.vanlissa@uu.nl>
2021-03-07 18:37:00 +0100

    fix test

--- tests/testthat/test-get_nodes_multigroup_label.R
+++ tests/testthat/test-get_nodes_multigroup_label.R
@@ -5,9 +5,8 @@ HS.model <- '  F =~ x1 + x2 + x3'
 fit <- cfa(HS.model,
            data = HolzingerSwineford1939,
            group = "school")
-p <- prepare_graph(fit)
-plot(p) + theme(aspect.ratio = 1)+ coord_fixed()
+
 test_that("get_nodes correctly labels multigroup models", {
-  expect_true(get_nodes(fit, label = "name")$label[1] == "speed")
+  expect_true(get_nodes(fit, label = "name")$label[1] == "F")
 })
 
