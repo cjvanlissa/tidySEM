06eb7777d3e5a1bda0373d450b221c3225eeaa20
cjvanlissa <c.j.vanlissa@tilburguniversity.edu>
2023-04-13 13:39:42 +0200

    update 2023-04-13 13:39:42

---
+++ R/mixture_predict.R
@@ -0,0 +1,34 @@
+#' @method predict MxModel
+#' @export
+predict.MxModel <- function(object,
+                            newdata,
+                            ...){
+  cl <- match.call()
+  cl[[1L]] <- str2lang("OpenMx:::predict.MxModel")
+  if(is.null(attr(object, "tidySEM"))){
+    eval.parent(cl)
+  } else {
+    switch(attr(object, "tidySEM"),
+           "mixture" = {
+             cl[[1L]] <- str2lang("predict_mxmodel_mixture")
+           })
+    eval.parent(cl)
+  }
+}
+
+predict_mxmodel_mixture <- function(object, newdata = NULL, ...){
+  if(is.null(newdata)){
+    return(class_prob(object, "individual")[[1]][,"predicted"])
+  } else {
+    mod_fix <- object
+    for(c in names(object@submodels)){
+      for(m in names(object[[c]]@matrices)){
+        mod_fix[[c]][[m]]$free[,] <- FALSE
+      }
+    }
+    mod_fix$data$observed <- rbind(newdata, mod_fix$data$observed)
+    mod_fix@matrices$weights$free[,] <- FALSE
+    mod_fix <- mxRun(mod_fix)
+    return(class_prob(mod_fix, "individual")[[1]][1:nrow(newdata), "predicted"])
+  }
+}
