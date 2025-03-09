6865406947b9314a515771f046487819baad3121
yourname <yourname@email.com>
2025-03-09 14:47:28 +0100

    update 2025-03-09 14:47:28.199987

--- R/igraph.R
+++ R/igraph.R
@@ -1,56 +1,27 @@
-# g <- make_ring(10)
-# edge_attr(g) <- list(
-#   name = LETTERS[1:10],
-#   color = rep("green", gsize(g))
-# )
-# edge_attr(g, "label") <- E(g)$name
-# plot(g)
-# get_nodes.igraph(g)
-#
-# g <- graph.data.frame(data.frame(one=1:2,two=2:3))
-# g <- set.vertex.attribute(g,'id',1,'first_id')
-# vertex_attr(g,'id',1)
-# plot(g)
-# get_nodes.igraph(g)
-#
-# names <- c('A','B','C,','D')
-# from <- c(113,115,112,114,113)
-# to <- c(112,112,115,113,114)
-# structure <- data.frame("from" = from, "to" = to)
-# g <- graph.data.frame(structure)
-# #V(g)$label <- names
-# V(g)$nonsense <- letters[seq_along(names)]
-# plot(g)
-# get_nodes(g)
-# unclass(g)
-# unclass(as_undirected(g))
-# as_directed(graph, mode = c("mutual", "arbitrary", "random", "acyclic"))
-
-# as_undirected(
-#   graph,
-#   mode = c("collapse", "each", "mutual"),
-#   edge.attr.comb = igraph_opt("edge.attr.comb")
-# )
-
 #' @method get_nodes igraph
+#' @importFrom igraph V as_data_frame
 #' @export
 get_nodes.igraph <- function(x, label = name, ...){
   label <- substitute(label)
   nod <- igraph::as_data_frame(x, what = "vertices")
-  nod$shape <- "none"
-  if(is.null(nod[["label"]])){
-    nod[["label"]] <- eval(label, envir = nod)
+  if(ncol(nod) == 0){
+    nod <- data.frame(name = as.character(unclass(igraph::V(x))))
   }
+  # att_nams <- vertex_attr_names(x)
+  if(is.null(nod[["shape"]])) nod[["shape"]] <- "none"
+  if(is.null(nod[["label"]])) nod[["label"]] <- eval(label, envir = nod)
+
   class(nod) <- c("tidy_nodes", class(nod))
   return(nod)
 }
 
 #' @method get_edges igraph
+#' @importFrom igraph as_data_frame
 #' @export
 get_edges.igraph <- function(x, label = NULL, ...){
   label <- substitute(label)
-  edg <- igraph::as_data_frame(g, "edges")
-  edg$arrow <- tryCatch({c("none", "last")[unclass(g)[[2]]+1L]}, error = function(e){
+  edg <- igraph::as_data_frame(x, "edges")
+  edg$arrow <- tryCatch({c("none", "last")[unclass(x)[[2]]+1L]}, error = function(e){
     message("Could not identify edge direction from object of class `igraph`.")
     "none"})
 
---
+++ tests/testthat/test-plot_igraph.R
@@ -0,0 +1,74 @@
+library(igraph)
+library(tidySEM)
+g <- make_ring(10)
+edge_attr(g) <- list(
+  name = LETTERS[1:10],
+  color = rep("green", gsize(g))
+)
+
+test_that("igraph edges correct", {
+  edg <- get_edges(g)
+  expect_true(all(c("from", "to", "name", "color", "arrow") %in% names(edg)) )
+  expect_true(all(edg$arrow == "none"))
+  edge_attr(g, "label") <- paste0("lab", E(g)$name)
+  edg <- get_edges(g)
+  expect_true(all(startsWith(edg$label, "lab")))
+})
+
+test_that("igraph nodes correct", {
+  nod <- get_nodes(g)
+  expect_true(all(c("name", "shape", "label") %in% names(nod)) )
+  expect_true(all(nod$shape == "none"))
+})
+
+test_that("igraph layout correct", {
+  lo <- get_layout(g)
+  expect_true(all(c("x", "y", "name") %in% names(lo)) )
+})
+
+g <- graph_from_data_frame(data.frame(one=1:2,two=2:3))
+g <- set_vertex_attr(g,'id',1,'first_id')
+
+test_that("igraph edges correct", {
+  edg <- get_edges(g)
+  expect_true(all(c("from", "to", "arrow") %in% names(edg)) )
+  expect_true(all(edg$arrow == "last"))
+})
+
+test_that("igraph nodes correct", {
+  nod <- get_nodes(g)
+  expect_true(all(c("name", "id", "shape", "label") %in% names(nod)) )
+})
+
+test_that("igraph layout correct", {
+  lo <- get_layout(g)
+  expect_true(all(c("x", "y", "name") %in% names(lo)) )
+})
+
+
+names <- c('A','B','C,','D')
+from <- c(113,115,112,114,113)
+to <- c(112,112,115,113,114)
+structure <- data.frame("from" = from, "to" = to)
+g <- graph.data.frame(structure)
+#V(g)$label <- names
+V(g)$nonsense <- letters[seq_along(names)]
+
+test_that("igraph edges correct", {
+  edg <- get_edges(g)
+  expect_true(all(c("from", "to", "arrow") %in% names(edg)) )
+  expect_true(all(edg$arrow == "last"))
+  g_un <- as_undirected(g)
+  edg_un <- get_edges(g_un)
+  expect_true(all(edg_un$arrow == "none"))
+})
+
+test_that("igraph nodes correct", {
+  nod <- get_nodes(g)
+  expect_true(all(c("name", "nonsense", "shape", "label") %in% names(nod)) )
+})
+
+test_that("igraph layout correct", {
+  lo <- get_layout(g)
+  expect_true(all(c("x", "y", "name") %in% names(lo)) )
+})
